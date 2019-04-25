b54345b931661d53ab28b53d91b3a3fbea0453c3
Mia Tran <mtran@MTran-MacBook-Pro.local>
2019-04-25 14:41:14 -0700

    custom post types

--- app/sql/local.wp_usermeta.sql
+++ app/sql/local.wp_usermeta.sql
@@ -2,10 +2,10 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_usermeta` VALUES
-(1,1,"nickname","mtran"),
-(2,1,"first_name",""),
-(3,1,"last_name",""),
-(4,1,"description",""),
+(1,1,"nickname","Mia Tran"),
+(2,1,"first_name","Mia"),
+(3,1,"last_name","Tran"),
+(4,1,"description","Testing hello 123."),
 (5,1,"rich_editing","true"),
 (6,1,"syntax_highlighting","true"),
 (7,1,"comment_shortcuts","false"),
@@ -17,6 +17,8 @@ INSERT INTO `wp_usermeta` VALUES
 (13,1,"wp_user_level","10"),
 (14,1,"dismissed_wp_pointers","wp496_privacy"),
 (15,1,"show_welcome_panel","1"),
-(16,1,"session_tokens","a:1:{s:64:\"534fbf072c290e76214d714a7964eee1dd0e59373f87a631389cca81cf34151e\";a:4:{s:10:\"expiration\";i:1555517993;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36\";s:5:\"login\";i:1555345193;}}"),
-(17,1,"wp_dashboard_quick_press_last_post_id","4"),
-(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}");
+(16,1,"session_tokens","a:1:{s:64:\"861c3b6682b9ce56582a06f54d536fe33307542044ec7d0b80e530f210535b13\";a:4:{s:10:\"expiration\";i:1556229917;s:2:\"ip\";s:10:\"172.17.0.1\";s:2:\"ua\";s:121:\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36\";s:5:\"login\";i:1556057117;}}"),
+(17,1,"wp_dashboard_quick_press_last_post_id","22"),
+(18,1,"community-events-location","a:1:{s:2:\"ip\";s:12:\"192.168.95.0\";}"),
+(19,1,"managenav-menuscolumnshidden","a:5:{i:0;s:11:\"link-target\";i:1;s:11:\"css-classes\";i:2;s:3:\"xfn\";i:3;s:11:\"description\";i:4;s:15:\"title-attribute\";}"),
+(20,1,"metaboxhidden_nav-menus","a:1:{i:0;s:12:\"add-post_tag\";}");
