5b5a23f6d825a0871a0db8915631b9950e955ea0
Mia Tran <mtran@MTran-MacBook-Pro.local>
2019-05-06 10:27:01 -0700

    creating relationships between content

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -24,6 +24,7 @@ function university_features() {
 add_action('after_setup_theme','university_features');
 
 function university_adjust_queries($query) {
+
     //make sure query will not apply on admin dashboard
     //make sure post type is 'event'
     //make sure the query being manipulated is the main query, so we don't accidentally manipulating any custom query
@@ -41,7 +42,14 @@ function university_adjust_queries($query) {
                     )    
                 )
             );
-    }
+    };
+
+    if (!is_admin() AND is_post_type_archive('program') AND $query->is_main_query()) {
+        $query->set('posts_per_page',-1);
+        $query->set('orderby','title');
+        $query->set('order','ASC');
+    };
+
 }
 
 add_action('pre_get_posts', 'university_adjust_queries');
