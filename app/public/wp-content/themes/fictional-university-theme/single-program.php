40b431f3f8e3f1bd7c06b3211954cff81e6ed716
Mia Tran <mtran@MTran-MacBook-Pro.local>
2019-05-06 11:15:17 -0700

    quick program edits

--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -19,6 +19,68 @@ while(have_posts()) {
               </p>
           </div>
       <div class="generic-content"><?php the_content() ?></div>
+
+      <?php
+       $today = date('Ymd');
+       $homepageEvents = new WP_Query(array(
+           'post_type' => 'event',
+           'posts_per_page' => 2,
+           'orderby' => 'meta_value_num', //meta_value_num is cutom field number value
+           'meta_key' => 'event_date', //specify meta_key as event date
+           'order' => 'ASC',
+           'meta_query' => array(
+             array(
+               'key' => 'event_date',
+               'compare' => '>=',
+               'value' => $today,
+               'type' => 'numeric'
+             ),
+             array(
+               'key' => 'related_programs',
+               'compare' => 'LIKE',
+               'value' => '"'. get_the_ID(). '"',
+             )
+           ),
+         )
+       );
+
+       if($homepageEvents->have_posts()) {
+        echo '<hr class="section-break">';
+        echo '<h2 class="headline headline--medium">Upcoming '  . get_the_title()  . ' Events</h2>';
+        while ($homepageEvents->have_posts()) {
+          $homepageEvents->the_post(); 
+          ?>
+            <div class="event-summary">
+              <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month">
+                <?php 
+                $eventDate = new DateTime(get_field('event_date'));
+                echo $eventDate->format('M');
+                ?>
+                </span>
+                <span class="event-summary__day">
+                <?php 
+                echo $eventDate->format('d');
+                ?></span>  
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php
+                if(has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 14);
+                }
+              ?><a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              </div>
+            </div>
+          <?php
+        } 
+        wp_reset_postdata();
+       }
+
+      ?>
+
     </div>
 <?php
     }
